6a13ca697ae1dfcf3e936204aa597444c7783bd6
Maher Douba <maherdouba@ariya-tech.net>
2024-08-01 13:10:12 +0300

    improved the error handling process and added typographical statements to verify storage operations

--- lib/data/repositories/pokemon_repository_impl.dart
+++ lib/data/repositories/pokemon_repository_impl.dart
@@ -1,5 +1,6 @@
+import 'dart:io';
 import 'package:dartz/dartz.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import '../../domain/entities/pokemon.dart';
 import '../../domain/repositories/pokemon_repository.dart';
 import '../datasources/pokemon_local_datasource.dart';
@@ -8,7 +9,7 @@ import '../datasources/pokemon_remote_datasource.dart';
 class PokemonRepositoryImpl implements PokemonRepository {
   final PokemonRemoteDataSource remoteDataSource;
   final PokemonLocalDataSource localDataSource;
-  
+
   PokemonRepositoryImpl({
     required this.localDataSource,
     required this.remoteDataSource,
@@ -17,21 +18,37 @@ class PokemonRepositoryImpl implements PokemonRepository {
   @override
   Future<Either<Exception, List<Pokemon>>> getAllPokemons() async {
     try {
-    
+      print('Trying to fetch from remote');
       final remotePokemons = await remoteDataSource.getAllPokemons();
-     
+      print('Fetched ${remotePokemons.length} pokemons from remote');
       await localDataSource.cachePokemons(remotePokemons);
+      print('Cached ${remotePokemons.length} pokemons');
       return Right(remotePokemons);
-    } on ClientException {
-      
+    } on http.ClientException catch (e) {
+      print('ClientException: ${e.toString()}');
       try {
+        print('Trying to fetch from local cache');
         final cachedPokemons = await localDataSource.getCachedPokemons();
+        print('Fetched ${cachedPokemons.length} pokemons from local cache');
         return Right(cachedPokemons);
       } catch (cacheException) {
+        print('CacheException: ${cacheException.toString()}');
+        return Left(Exception('Failed to fetch pokemons from local data source: ${cacheException.toString()}'));
+      }
+    } on HandshakeException catch (e) {
+      print('HandshakeException: ${e.toString()}');
+      try {
+        print('Trying to fetch from local cache');
+        final cachedPokemons = await localDataSource.getCachedPokemons();
+        print('Fetched ${cachedPokemons.length} pokemons from local cache');
+        return Right(cachedPokemons);
+      } catch (cacheException) {
+        print('CacheException: ${cacheException.toString()}');
         return Left(Exception('Failed to fetch pokemons from local data source: ${cacheException.toString()}'));
       }
     } catch (exception) {
-      return Left(Exception('Failed to fetch pokemons: ${exception.toString()}'));
+      print('General Exception: ${exception.toString()}');
+      return Left(Exception('Failed to fetch pokemons from data source: ${exception.toString()}'));
     }
   }
 }
